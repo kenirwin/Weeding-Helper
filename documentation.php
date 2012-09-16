c60b9386c732eb25b410586c32b01349769279e3
Ken Irwin <kirwin@wittenberg.edu>
2012-09-16 14:09:59 -0400

    more improved docs

--- documentation.php
+++ documentation.php
@@ -28,12 +28,14 @@ div.example {
    <li>Unzip the file in a directory of its own. (The unzipping process creates a lot of files in the current directory, rather than a new directory with a lot of files in it.)</li>
      <li>Rename or copy the <b>config-sample.php</b> to be <b>config.php</b></li>
      <li>Edit the <b>config.php</b> file to include your local MySQL username, password, and the name you wish to use for the msyql database associated with Weeding Helper.</li>
-					   <li>On the Linux command-line, change the permissions on the <b>upload/</b> and <b>prepped/</b> directories to be writeable by the web server AND by the user who under whose name the <b>cron</b> processes will run (see step 5). In our library, this has been achieved by making a permission group called &quot;apache&quot;, containing both the web-process-user and the chief implementor&#39;s own username; This command will probably look something like: <br />
+					   <li>On the Linux command-line, change the permissions on the <b>upload/</b> and <b>prepped/</b> directories to be writeable by the web server AND by the user who under whose name the <b>cron</b> processes will run (see step 6). In our library, this has been achieved by making a permission group called &quot;apache&quot;, containing both the web-process-user and the chief implementor&#39;s own username; This command will probably look something like: <br />
 <div class="example">
    <code>&gt; chgrp apache upload/</code><br />
    <code>&gt; chgrp apache prepped/</code><br />
 </div>
 where "apache" is the name of a permissions group that grants write-permissions to the web server and includes the user who sets up the cron job in the next step.</li>
+									 <li>Make sure the two files <b>prep_file.php</b> and <b>innreach_check.php</b> begin with valid pointers to where PHP is running on your server. By default, they will start with this declaration: <b>#!/usr/bin/php</b> -- that will work on many servers, but not all. To find a valid php path, type on the command line <b>which php</b></li>
+
    <li>Set up the <code>cron</code> jobs that power Weeding Helper&apos;s automated functions. Cron is a unix utility that can run scripts on a regular basis; if you hare unfamiliar with cron, you will want to get help from a system administrator or similar. The two files that will need to be activated by cron jobs are <b>prep_file.php</b> and <b>innreach_check.php</b>. Running the cronjobs once per minute is an effective approach.
    <ol class="sublist">
    <li><b>prep_file.php</b> takes an already-uploaded file loads it into the database. That only needs to happen once per file. If that cron job runs once per minute, it will usually not need to do anything; but when a file is uploaded, it will reliably be added to the database very quickly.</li>
@@ -61,3 +63,31 @@ where "apache" is the name of a permissions group that grants write-permissions 
    </ol>
 <a name="how_to_add_circ_data"></a>
 <h2>How to Upload Files</h2>
+																											<p>To upload new data from a III Review File, export the data in this format (order matters!):</p>
+<table>
+<thead>
+ <tr><th>Field Type</th> <th>Field</th></tr>
+</thead>
+<tbody>
+<tr><td>Bibliographic</td><td>Author</td></tr>
+<tr><td>Bibliographic</td><td>Title</td></tr>
+<tr><td>Bibliographic</td><td>Imprint</td></tr>
+<tr><td>Bibliographic</td><td>Subject</td></tr>
+<tr><td>Bibliographic</td><td>Cat Date</td></tr>
+<tr><td>Bibliographic</td><td>Location</td></tr>
+<tr><td>Bibliographic</td><td>Call #</td></tr>
+<tr><td>Bibliographic</td><td>BCODE3</td></tr>
+<tr><td>Bibliographic</td><td>MAT TYPE</td></tr>
+<tr><td>Bibliographic</td><td>RECORD #</td></tr>
+<tr><td>Item</td><td>Tot Chkout</td></tr>
+<tr><td>Item</td><td># Renewals</td></tr>
+<tr><td>Item</td><td>Intl Use</td></tr>
+<tr><td>Item</td><td>Lchkin</td></tr>
+</tbody>
+</table>
+
+<p>Field Delimiter: tab (aka: Control Character 9 in Millennium)<br>
+Text Qualifier: none<br>
+Repeated Field Delimiter: ; (semi-colon)<br>
+Max field length: none</p>
+
