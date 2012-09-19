51c8b1ed322f9d5e62f3e8eb26bc4a6b9c4335fa
Ken Irwin <kirwin@wittenberg.edu>
2012-09-18 20:23:24 -0400

    further tidying of documentation

--- attribution.php
+++ attribution.php
@@ -1,5 +1,4 @@
 <?
-
 $nouns = array ("'Cloud' by The Noun Project" => "Attribution (CC BY 3.0)",
 		"'Printer' by The Noun Project" => "Attribution (CC BY 3.0)",
 		"'Pencil' by d" => "No Rights Reserved (CC0)",
@@ -14,5 +13,5 @@ print "<dt>All icons are copied or adapted from images in <a href=\"http://www.t
 foreach ($nouns as $noun => $license) {
   print "<dd>$noun; Creative Commons License: $license</dd>\n";
 }
-
 ?>
+<dd>The the Weeding Helper logo incorporates 'Sprout' and 'Handwashing' described above and is a ridiculously lousy logo, all things considered, but it is also licensed under the "Attribution (CC BY 3.0)" license, attribution to Ken Irwin for the whole and Tak Imoto & Igor Kiselev for individual graphic elements.</dd>
