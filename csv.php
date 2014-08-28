7e14aaf78f2ecb8257ad04b53a9a8ba42b78069b
Ken Irwin <kirwin@wittenberg.edu>
2014-08-28 12:20:35 -0400

    index.php points to controller.php to avoid previous silly duplication; tidied csv code

--- csv.php
+++ csv.php
@@ -1,4 +1,9 @@
 <?php 
+  /*
+    The CSV generating code here is a modified version of Jrgns's code:
+    http://stackoverflow.com/a/125578
+   */
+
 session_start();
 include ("config.php");
 include ("mysql_connect.php");
@@ -32,40 +37,5 @@ if ($fp && $r) {
   }
   die;
 }
-
-
-/*
-$columns_total = mysql_num_fields($q);
-
-// Get The Field Name
-
-for ($i = 0; $i < $columns_total; $i++) {
-  $heading = mysql_field_name($sql, $i);
-
-  $output .= '"'.$heading.'",';
-}
-$output .="\n";
-
-// Get Records from the table
-
-while ($row = mysql_fetch_array($sql)) {
-  for ($i = 0; $i < $columns_total; $i++) {
-    $output .='"'.$row["$i"].'",';
-    
-  }
-  $output .="\n";
-}
-
-// Download the file
-
-$filename = "$table.csv";
-header('Content-type: application/csv');
-header('Content-Disposition: attachment; filename='.$filename);
-
-echo $output;
-exit;
-
-*/
-
 
 ?>
